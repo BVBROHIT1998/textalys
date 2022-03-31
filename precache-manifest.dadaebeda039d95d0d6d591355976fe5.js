2c80ea36f8c6262b7eb85a47c1793081a683fc49
BVBROHIT1998 <46211619+BVBROHIT1998@users.noreply.github.com>
2022-03-31 14:37:30 +0530

    Updates

---
+++ precache-manifest.dadaebeda039d95d0d6d591355976fe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc36b2f06cbddb018798",
+    "url": "/textalys/static/css/main.1f6a3f23.chunk.css"
+  },
+  {
+    "revision": "bc36b2f06cbddb018798",
+    "url": "/textalys/static/js/main.bc36b2f0.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/textalys/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "b619f7d5883ab0fa0633",
+    "url": "/textalys/static/js/2.b619f7d5.chunk.js"
+  },
+  {
+    "revision": "7ce6122c1af9a68d9cca",
+    "url": "/textalys/static/js/runtime~main.7ce6122c.js"
+  },
+  {
+    "revision": "815385722c12e2ffbd570645a7cbf223",
+    "url": "/textalys/index.html"
+  }
+];
